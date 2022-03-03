2820bbe1b4098e8da98d3753540b0fb7556d2727
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-03-03 14:20:37 +0000

    Setup initial function structure

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+def introduce_game():
+    """
+    Displays a greeting and prompts the user to choose a name
+    """
+    print("-" * 30)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    introduce_game()
+
+
+main()
