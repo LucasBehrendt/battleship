cb5d61626480fd8f3c67dd73efab919f61553715
LucasBehrendt <Lu.Behrendt@Hotmail.com>
2022-03-04 13:33:36 +0000

    Add class for text colors & add ascii art

--- run.py
+++ run.py
@@ -1,11 +1,40 @@
 import random
 
 
+class Colours:
+    """
+    Set colours to use throughout the game
+    Source: https://stackoverflow.com/questions/287871/
+    """
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def introduce_game():
     """
     Displays a greeting and prompts the user to choose a name
+    Ascii art source:
+    https://patorjk.com/software/taag/#p=display&f=Doom&t=Battleship
     """
-    print("-" * 30)
+    print(f"""{Colours.OKBLUE}
+______       _   _   _           _     _
+| ___ \     | | | | | |         | |   (_)
+| |_/ / __ _| |_| |_| | ___  ___| |__  _ _ __
+| ___ \/ _` | __| __| |/ _ \/ __| '_ \| | '_ \ 
+| |_/ / (_| | |_| |_| |  __/\__ \ | | | | |_) |
+\____/ \__,_|\__|\__|_|\___||___/_| |_|_| .__/
+                                        | |
+                                        |_|
+    {Colours.ENDC}""")
+    print("-" * 80)
+    print("\nWelcome to Battleship! \n")
 
 
 def main():
